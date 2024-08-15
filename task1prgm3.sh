d8cad1cd8450aefff2872dc297cf7c6e57db4f98
Sujith Gopi R <rsujithgopi@gmail.com>
2024-08-15 12:30:49 +0530

    Task2

--- task1prgm3.sh
+++ task1prgm3.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-rm -rf * # removing all the files recursively and forcibly here r means recursively and f means forcibly
+cwd=$(pwd) #getting the current working directory
+find $cwd -type f -name "*.txt" -exec rm {} + # removing all the text files recursively and forcibly, here f means forcibly
