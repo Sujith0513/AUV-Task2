718c2e8b01a0b47679b07bb3c806609f4bdacf7f
Sujith0513 <155612522+Sujith0513@users.noreply.github.com>
2024-08-16 00:34:54 +0530

    Add files via upload

--- task1prgm5.sh
+++ task1prgm5.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 read -p "Enter the base of the triangle : " n
-read -p "Enter the position of the element when divided by 10 : " pos
+#read -p "Enter the position of the element :" pos
+pos=1
 declare -i y=1
 declare -i z=-1
 declare -i end=0
@@ -13,7 +14,7 @@ do
 	start=z+1 # start is the number value at the start of the line
 	for j in $(seq $start $end)
 	do	
-		if [[ $((j/10)) > 0 ]] # if j is greater than 10, we can get the xth position
+		if [[ $((j/10)) > 0 ]] # if j is greater than 10 as it should be divisible by 10 to get the position
 		then
 			echo -n $(( (j/(10**($pos-1)))%10 )) #getting the xth position by removing the number before the xth position and using modulus function to get the number
 		else
